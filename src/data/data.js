c68655731d95b51a17116c614fa57c7be5e9748e
Mher <mher.manukyan.2019@gmail.com>
2023-05-14 19:01:13 +0300

    first

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import WorkIm from '../assets/projects/workImg.jpg'
+import RealEst from '../assets/projects/realestate.jpg'
+import autoImg from "../assets/projects/autoImg.jpg"
+export const data=[
+    {
+        id:1,
+        name:"Food app",
+        image:WorkIm,
+        github:"https://github.com/MherManukyan/foodweb",
+        live:"https://foodweb-xi.vercel.app/",
+    },
+    {
+        id:2,
+        name:"Real Estate App",
+        image:RealEst,
+        github:"https://github.com/MherManukyan/LandBroking",
+        live:"https://land-broking.vercel.app/",
+    },
+    {
+        id:3,
+        name:"Auto rent app",
+        image:autoImg,
+        github:"https://github.com/MherManukyan/cars-hw",
+        live:"https://mhermanukyan.github.io/cars-hw/",
+    },
+  
+
+
+]
